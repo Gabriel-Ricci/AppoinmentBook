0a149bf4b86656f6e09e91cc27464cc86b5ea8bd
Gabriel-Ricci <gabrielr244@nycstudents.net>
2024-11-13 14:32:11 +0000

    p

--- AppoinmentBook.java
+++ AppoinmentBook.java
@@ -5,4 +5,15 @@ public class AppoinmentBook {
     {
         this.schedule = schedule; 
     }
+    private void reserveBlock(int period, int startMinute, int duration) {
+    }
+    private boolean isMinuteFree(int period, int minute) {
+        return schedule[period - 1][minute];
+    }
+    public int findFreeBlock(int period, int duration) {
+        return 0;
+    }
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+        return false;
+    }
 }
